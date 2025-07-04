e920d284379e1c376f594e5857b1001505878bce
mateoSebF <mateo.forero-f@mail.escuelaing.edu.co>
2025-07-04 09:57:45 -0500

    feature: initial base proyect

---
+++ src/main/java/com/eci/iagen/schedule_compliance/ScheduleComplianceApplication.java
@@ -0,0 +1,13 @@
+package com.eci.iagen.schedule_compliance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleComplianceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleComplianceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eci/iagen/schedule_compliance/ScheduleComplianceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eci.iagen.schedule_compliance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleComplianceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
