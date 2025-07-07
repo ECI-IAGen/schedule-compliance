068a43cc3b16fb9361481d772fe374a9d6a577c8
mateoSebF <mateo.forero-f@mail.escuelaing.edu.co>
2025-07-07 11:37:44 -0500

    feat: Inicio base conexion microservicio

--- src/main/java/com/eci/iagen/schedule_compliance/ScheduleComplianceApplication.java
+++ src/main/java/com/eci/iagen/schedule_compliance/ScheduleComplianceApplication.java
@@ -3,10 +3,27 @@ package com.eci.iagen.schedule_compliance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ScheduleComplianceApplication {
 
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ScheduleComplianceApplication.class);
+
 	public static void main(String[] args) {
+		// Load environment variables from .env file only if not already set
+		try {
+			Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+			dotenv.entries().forEach(entry -> {
+				String key = entry.getKey();
+				
+				// Only set the system property if it is not already set
+				logger.info("Setting system property: {}={}", key, entry.getValue());
+				System.setProperty(key, entry.getValue());
+			});
+		} catch (Exception e) {
+			// Continue without .env file
+		}
 		SpringApplication.run(ScheduleComplianceApplication.class, args);
 	}
 
